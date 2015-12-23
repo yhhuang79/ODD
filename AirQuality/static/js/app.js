31875bb66bd3a987333fbc2b3df6b9c07bad43dd
yuhsiang <yhhuang79@hotmail.com>
2015-12-23 16:27:23 +0800

    fix datetime format

--- AirQuality/static/js/app.js
+++ AirQuality/static/js/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $.get("/ODD/status/", function(data){
-        alert(JSON.stringify(data));
+        //alert(JSON.stringify(data));
         var tableContent = [];
         $.each(data, function(i, d) {
             var dd = new Date();
@@ -12,7 +12,7 @@ $(document).ready(function(){
                         <th></th>\
                         <th>" + d.total_size + "</th>\
                         <th>" + d.eachtime + "</th>\
-                        <th>" + dd.getFullYear()+"/"+dd.getMonth()+1+"/"+dd.getDate()+" "+dd.getHours()+":"+dd.getMinutes()+":"+dd.getSeconds() + "</th>\
+                        <th>" + dd.getFullYear()+"/"+(parseInt(dd.getMonth())+1) +"/"+dd.getDate()+" "+dd.getHours()+":"+dd.getMinutes()+":"+dd.getSeconds() + "</th>\
                         <th>" + d.start_time.toString() + "</th>\
                         <th>" + d.finish_time + "</th>\
                         <th>" + d.download_duration + "</th>\
